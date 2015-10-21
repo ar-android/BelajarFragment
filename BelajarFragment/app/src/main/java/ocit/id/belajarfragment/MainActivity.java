119069effec4441b1fd92d0350e5d80b8e005e0d
ar-android <ocittwo@gmail.com>
2015-10-22 03:46:40 +0700

    Belajar fragment editted

--- BelajarFragment/app/src/main/java/ocit/id/belajarfragment/MainActivity.java
+++ BelajarFragment/app/src/main/java/ocit/id/belajarfragment/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import ocit.id.belajarfragment.adapter.AdapterFragmentTab;
 
@@ -11,11 +12,15 @@ public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         tabLayout = (TabLayout)findViewById(R.id.tabViewPager);
         viewPager = (ViewPager)findViewById(R.id.pager);
